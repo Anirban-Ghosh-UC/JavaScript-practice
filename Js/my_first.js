65c977bbc3137bd1d0d0bb179cd1f76177355ccd
Anirban-Ghosh-UC <anirbangh669@gmail.com>
2021-06-19 13:32:56 +0530

    First Commit

---
+++ Js/my_first.js
@@ -0,0 +1,22 @@
+// var x=1
+// a();
+// b();
+// console.log(x)
+// function a(){ 
+//   var x=10
+//   console.log(x)
+// }
+
+// function b(){ 
+//     var x=100
+//     console.log(x)
+//   }
+
+
+/// undefined vs not defined
+// console.log(a)
+// var a=10;
+// console.log(a)
+// console.log(x)
+
+document.write("<h1>External File</h1>");
